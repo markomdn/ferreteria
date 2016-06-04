5546a94ba2c6d96eb236b062238b4b4bf72d38a8
markomdn <markomdn@gmail.com>
2016-06-03 21:46:30 -0300

    stable

--- public/index.php
+++ public/index.php
@@ -58,7 +58,3 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
